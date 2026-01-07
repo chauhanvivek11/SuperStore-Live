b799c186e1c0f6b5e00fc526427aa9dd3fca5e26
Vivek Chauhan <vivekchauhan11mg@gmail.com>
2026-01-07 23:52:12 +0530

    Adding Things That Are Neccessary

---
+++ ShopBackend/GlobalUsings.cs
@@ -0,0 +1,16 @@
+// Ye file saare missing references ko theek karegi
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using MongoDB.Bson;
+global using MongoDB.Bson.Serialization.Attributes;
+global using MongoDB.Driver;
